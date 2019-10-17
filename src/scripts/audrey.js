0c6b0b9f5e21d2cb7237e4ae3244be13e4070006
Adam Byrd <ajbyrd0723@gmail.com>
2019-10-17 09:35:41 -0500

    initial function works

--- src/scripts/audrey.js
+++ src/scripts/audrey.js
@@ -4,11 +4,14 @@ const audrey = document.getElementById("audrey")
     Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
-audrey.addEventListener("scroll", function () {
+window.addEventListener("scroll", function () {
     /*
         Adjust the width of audrey to be 1/3 the value of
         `window.scrollY`. No lower than 50px, though.
     */
+
+    
+/* console.log("This is working")*/
 
 
 
